7311ba314d0ccbd71f92a58a02532ec40c7e6ab3
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-11-18 15:05:36 -0300

    Inicialization on vue

---
+++ src/components/header.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+<template>
+    <section class="container-header">
+        <div>
+            <h1>RUMBLE</h1>
+            <img class="logo" src="../../public/rumble-logo.png">
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Series</li>
+                <li>Filmes</li>
+            </ul>
+        </div>
+        <div>
+            <div>
+                <input type="text" placeholder="Procure Filmes , Series...">
+                <img class="lupa" src="../../public/lupa-logo.png">
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+    .container-header {
+        height: 4rem;
+        background-color: black;
+    }
+
+    .logo {
+        width: 3rem;
+        height: 3rem;   
+    }
+    .lupa {
+        width: 2rem;
+        height: 2rem; 
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import headerVue from "@/components/header.vue";
+</script>
+<template>
+    <headerVue />
+</template>
+<style scoped>
+    
+</style>
